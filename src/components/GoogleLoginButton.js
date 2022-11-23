ed306cca16559f82ed5b8626a23fc5bd1b977f0a
priyanshukaushal <priyanshu.kaushal2001@gmail.com>
2022-11-24 01:18:27 +0530

    google login added

---
+++ src/components/GoogleLoginButton.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { gapi } from 'gapi-script';
+
+function GoogleLoginButton() {
+    const [ profile, setProfile ] = useState([]);
+    const clientId = '747114995861-mq5jik2m36mp50b1dcb5oi5ilam1nvrp.apps.googleusercontent.com';
+    useEffect(() => {
+        const initClient = () => {
+            gapi.client.init({
+                clientId: clientId,
+                scope: 'https://www.googleapis.com/auth/userinfo.profile'
+            });
+        };
+        gapi.load('client:auth2', initClient);
+    });
+
+    const onSuccess = (res) => {
+        setProfile(res.profileObj);
+        console.log(res.profileObj);
+    };
+
+    const onFailure = (err) => {
+        console.log('failed', err);
+    };
+
+    const logOut = () => {
+        setProfile(null);
+    };
+
+    return (
+        <div>
+            {profile ? (
+                <div>
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
+                </div>
+            ) : (
+                <GoogleLogin
+                    clientId={clientId}
+                    buttonText="Log In"
+                    onSuccess={onSuccess}
+                    onFailure={onFailure}
+                    cookiePolicy={'single_host_origin'}
+                    isSignedIn={true}
+                />
+            )}
+        </div>
+    );
+}
+export default GoogleLoginButton;
