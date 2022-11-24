d62c74de2b75b17d60218da49f7e1178f8e54cd6
akshit2382 <akshitj018@gmail.com>
2022-11-25 00:46:14 +0530

    code generator added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Profile from "./components/Profile/Profile";
 import GoogleLoginButton from "./components/GoogleLoginButton";
+import CodeGenerator from "./components/CodeGenerator/CodeGenerator2";
 function App() {
   return (
     <div className="App">
@@ -10,9 +11,10 @@ function App() {
         <Nav />
         <Routes>
           <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/codegenerator" element={<CodeGenerator />} />
         </Routes>
       </Router>
-      <GoogleLoginButton></GoogleLoginButton>
+      {/* <GoogleLoginButton></GoogleLoginButton> */}
     </div>
   );
 }
