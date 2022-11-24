aab3c67288c06d1198ae371bc411ee5866ebbf72
Arjun Kathail <arjunkathail5@gmail.com>
2022-11-25 00:14:54 +0530

    Landing Page background

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 export const Home = () => {
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <div className={styles.landingBg}>
+        <div className={styles.introduction}>
+          <h1>Home page</h1>
+        </div>
+      </div>
+    </>
   );
 };
