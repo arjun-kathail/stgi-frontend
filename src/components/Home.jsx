186836a72d95e27bc52d1cb15e52dd005505b8b9
Arjun Kathail <arjunkathail5@gmail.com>
2022-11-25 08:56:08 +0530

    Remove unnecessary dependencies

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect } from "react";
+import { React } from "react";
 import styles from "./Home.module.css";
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from "react-router-dom";
